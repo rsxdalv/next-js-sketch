3f7793a597ad5817e193b3a51c05edb5f8193937
Roberts Slisans <rsxdalv@gmail.com>
2021-07-28 12:55:44 +0300

    Static Generation

---
+++ pages/statically-generated.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+export default function StaticallyGenerated({ data, date }) {
+    return (
+        <div>
+            <Head>
+                <title>Statically Generated Props</title>
+            </Head>
+            <main>
+                <ul>
+                    <li>Data payload: {data}</li>
+                    <li>Generation Date: {date}</li>
+                </ul>
+            </main>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    // Get external data from the file system, API, DB, etc.
+    const data = Math.PI;
+
+    // The value of the `props` key will be
+    //  passed to the `Home` component
+    return {
+        props: {
+            data,
+            date: new Date().toDateString() + ' ' + new Date().toTimeString()
+        }
+    }
+}
